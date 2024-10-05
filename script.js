22d88544e82c62622266365a7ae818ebea24be3d
Kevin Roque <88600617+paperMan07@users.noreply.github.com>
2024-10-05 10:33:02 -0400

    Created script.js
    Coded getComputerChoice function, started getHumanChoice

---
+++ script.js
@@ -0,0 +1,23 @@
+//Declare function getComputerChoice
+function getComputerChoice() {
+//Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
+let computerChoice = Math.floor(Math.random()*3)
+if (computerChoice == 1){
+    return 'rock'
+} else if (computerChoice == 2){
+return 'paper'
+}
+else
+{return "scissors"}
+}
+
+//Declare a function called getHumanChoice
+function getHumanChoice(){
+//Write code so that getHumanChoice will return one of the valid choices  
+let HumanChoice = prompt("poo?")
+
+}
+//Write so that getHumanChoice will return one of the valid choices
+//depeding on the user inpit
+
+prompt("poo?")
